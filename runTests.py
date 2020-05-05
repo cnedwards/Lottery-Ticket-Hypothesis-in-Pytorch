ff1afa0ec84c1d3fe87d56e6b2e85201285d98eb
Carl <cedwar45@utk.edu>
2020-05-05 05:04:29 -0400

    Pushing some mnist combined tests. Corrected test code. It was using all 7 MNIST tickets.
    This caused errors because we were, for example, getting 7*6*5 permutations of size 3.

--- runTests.py
+++ runTests.py
@@ -3,8 +3,8 @@ from itertools import permutations, combinations
 
 mnist_ticket_path = 'tickets/mnist/'
 fmnist_ticket_path = 'tickets/fmnist/'
-mnist_tickets = os.listdir(mnist_ticket_path)
-fmnist_tickets = os.listdir(fmnist_ticket_path)
+mnist_tickets = os.listdir(mnist_ticket_path)[0:4]
+fmnist_tickets = os.listdir(fmnist_ticket_path)[0:4]
 
 program = "python merge_tickets.py --prune_type=lt --arch_type=fc1 --dataset="
 param2 = " --prune_percent=10 --end_iter=20 --ticket_folders="
@@ -14,14 +14,20 @@ param2 = " --prune_percent=10 --end_iter=20 --ticket_folders="
 dataset = "mnist"
 result_path = "test_results/merge/" + dataset + "/"
 
+''' #skip now because messed up code.
 p2 = permutations(mnist_tickets, 2)
 for p in list(p2):
     cmd = str(program + dataset + param2 + mnist_ticket_path + p[0] + "," + mnist_ticket_path + p[1] + " --output_folder=" + result_path + p[0] + p[1])
     os.system(cmd)
     print(cmd)
+'''
 
 p3 = permutations(mnist_tickets, 3)
 for p in list(p3):
+    if (os.path.isdir("test_results/merge/mnist/"+ p[0] + p[1] + p[2])): #check if we already ran this merge
+        print("Skipping: ", p[0] + p[1] + p[2])
+        continue 
+    
     cmd = str(program + dataset + param2 + mnist_ticket_path + p[0] \
         + "," + mnist_ticket_path + p[1] \
         + "," + mnist_ticket_path + p[2] + " --output_folder=" + result_path + p[0] + p[1] + p[2])
